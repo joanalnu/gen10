246544cfbdb2c05fd6f6515a29080ced7f5ee339
joanalnu <joanalnu@outlook.com>
2024-12-06 14:49:28 +0100

    issue with index.lock

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,7 @@
 import pytest
 import gen_api
+from build.lib.gen_api import dna_schneiden
+
 
 def test_dna2rna():
     assert gen_api.dna2rna("TACCACGTGGACTGAGGACTCCTCATT") == "AUGGUGCACCUGACUCCUGAGGAGUAA"
@@ -19,3 +21,19 @@ def test_dna2amino():
 def test_rna2amino():
     rna = "AUGGUGCACCUGACUCCUGAGGAGUAA"
     assert gen_api.rna2amino(rna) == " Met Val His Leu Thr Pro Glu Glu"
+
+def test_check():
+    dnas = ['TACCACGTGGACTGAGGACTCCTCATT', 'TACCACGTGGACTGAGGACTCCTCATC', 'TACCACGTGGACTGAGGACTCCTCACC']
+    rnas = ['AUGGUGCACCUGACUCCUGAGGAGUAA', 'AUGGUGCACCUGACUCCUGAGGAGUAG', 'AUGGUGCACCUGACUCCUGAGGAGUGG']
+    for seq in dnas:
+        result = gen_api.check(seq)
+        assert result == 'Valid DNA string'
+
+    for seq in rnas:
+        result = gen_api.check(seq)
+        assert result == 'Valid RNA string'
+
+    lst = ['AUGGUGCACCGACUCCUGAGGAGUA', 'AUGGUGCACCUGACUCCUGAGGAGUAA', 'AUGGUGCACCUGACUCCUGAGGAGUAT']
+    for seq in lst:
+        result = gen_api.check(seq)
+        assert result == 'Invalid string (startin/ending codons not found)'
