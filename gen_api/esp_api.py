9ede465bf4752b199b9fda2e9c59b442df7ee0de
joanalnu <joanalnu@outlook.com>
2025-01-18 13:17:15 +0100

    add new check_codon function to other languages

--- gen_api/esp_api.py
+++ gen_api/esp_api.py
@@ -313,3 +313,55 @@ def buscar(string, sequence):
         start_index += 1  # Move to the next possible starting position
 
     return occurrences
+
+def check_codon(string):
+    if string=='':
+        raise ValueError("La cadena está vacía, compruebe su input.")
+
+    # Check if string length is divisible by 3
+    if len(string) % 3 != 0:
+        if len(string) < 3:
+            raise ValueError(f"La cadena no se puede dividir en codones: {string}")
+        if len(string) % 2 == 0:
+            resting = string[-1]
+        else:
+            resting = string[-2:]
+        raise ValueError(f"La cadena no se puede dividir en codones sin el siguiente resto: {resting}")
+
+    if 'u' not in string or 'U' not in string:
+        stringmem = string
+        string = ''
+        for letter in stringmem:
+            try:
+                string += gen_api.dna2rna(letter)
+            except ValueError:
+                string += letter
+
+    # Define codon catalog
+    codon_catalog = {
+        'UUU': 'Phe', 'UUC': 'Phe', 'UUA': 'Leu', 'UUG': 'Leu',
+        'UCU': 'Ser', 'UCC': 'Ser', 'UCA': 'Ser', 'UCG': 'Ser',
+        'UAU': 'Tyr', 'UAC': 'Tyr', 'UAA': 'STOP', 'UAG': 'STOP',
+        'UGU': 'Cys', 'UGC': 'Cys', 'UGA': 'STOP', 'UGG': 'Trp',
+        'CUU': 'Leu', 'CUC': 'Leu', 'CUA': 'Leu', 'CUG': 'Leu',
+        'CCU': 'Pro', 'CCC': 'Pro', 'CCA': 'Pro', 'CCG': 'Pro',
+        'CAU': 'His', 'CAC': 'His', 'CAA': 'Gln', 'CAG': 'Gln',
+        'CGU': 'Arg', 'CGC': 'Arg', 'CGA': 'Arg', 'CGG': 'Arg',
+        'AUU': 'Ile', 'AUC': 'Ile', 'AUA': 'Ile', 'AUG': 'Met',
+        'ACU': 'Thr', 'ACC': 'Thr', 'ACA': 'Thr', 'ACG': 'Thr',
+        'AAU': 'Asn', 'AAC': 'Asn', 'AAA': 'Lys', 'AAG': 'Lys',
+        'AGU': 'Ser', 'AGC': 'Ser', 'AGA': 'Arg', 'AGG': 'Arg',
+        'GUU': 'Val', 'GUC': 'Val', 'GUA': 'Val', 'GUG': 'Val',
+        'GCU': 'Ala', 'GCC': 'Ala', 'GCA': 'Ala', 'GCG': 'Ala',
+        'GAU': 'Asp', 'GAC': 'Asp', 'GAA': 'Glu', 'GAG': 'Glu',
+        'GGU': 'Gly', 'GGC': 'Gly', 'GGA': 'Gly', 'GGG': 'Gly'
+    }
+
+    # Find invalid codons
+    invalid_codons = []
+    for i in range(0, len(string), 3):
+        codon = string[i:i+3]
+        if codon not in codon_catalog:
+            invalid_codons.append(codon)
+
+    return invalid_codons
